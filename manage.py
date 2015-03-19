3d818d5e9b1eaca949020add42428b054717fd78
1 <grdesu@gmail.com>
2015-03-19 21:32:51 +0600

    At least this shit works now

--- manage.py
+++ manage.py
@@ -2,7 +2,9 @@ from flask import Flask, jsonify
 from flask.ext.restful import reqparse, abort, Api, Resource
 from flask.ext.mongoalchemy import MongoAlchemy
 from flask.ext.sqlalchemy import SQLAlchemy
-import os
+import os, string, random, time
+from functools import wraps
+from passlib.apps import custom_app_context as pwd_context
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
@@ -12,14 +14,47 @@ db = SQLAlchemy(app)
 api = Api(app)
 parser = reqparse.RequestParser()
 parser.add_argument('name', type=str)
+parser.add_argument('pwd', type=str)
 parser.add_argument('surname', type=str)
 parser.add_argument('state', type=str)
 parser.add_argument('change-balance', type=int)
+parser.add_argument('session', type=str)
 
 def get_client(client_id):
 	if ClientModel.query.get(client_id) is None:
 		abort(404, message="Client {} doesn't exist".format(client_id))
-	return ClientModel.query.get(client_id)
+	return ClientModel.query.get(client_id)		
+		
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(100))
+	password_hash = db.Column(db.String(100))
+
+	def hash_password(self, password):
+		self.password_hash = pwd_context.encrypt(password)
+
+	def verify_password(self, password):
+		return pwd_context.verify(password, self.password_hash)
+
+	def dict(self):
+		return {'username': self.username}
+
+class Sess(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	start = db.Column(db.Integer)
+	session = db.Column(db.String(30))
+	
+	def dict(self):
+		return {'session': self.session}
+		
+def login_required(session):
+	session = Sess.query.filter(Sess.session == session).first()
+	if (session):
+		if (time.time() - session.start) < 60:
+			return True
+		abort (400, message = 'Session expired')
+	abort (403)
+		
 
 class ClientModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,7 +67,6 @@ class ClientModel(db.Model):
         self.name = name
         self.surname = name
         self.state = state
-        #self.balance = balance
 
     def __repr__(self):
         return ('name: ' + self.name + ', state: ' + self.state)
@@ -41,10 +75,11 @@ class ClientModel(db.Model):
 		return {'id': self.id, 'name':self.name,'surname':self.surname, 'balance':self.balance, 'state':self.state}
 
 class Client(Resource):
+	#method_decorators=[authenticate]
 	def get(self, client_id):
 		return get_client(client_id).dict()
 
-	def delete(self, client_id):
+	def delete(self, clienlot_id):
 		db.session.delete(get_client(client_id))
 		db.session.commit()
 		return '', 204
@@ -57,14 +92,20 @@ class Client(Resource):
 		return client.dict()
 
 class ClientsList(Resource):
+	#method_decorators=[authenticate]
 	def get(self):
-		clientList = []
-		page = 1
-		ipp = 10
-		paginate = ClientModel.query.paginate(page, ipp)
-		for client in ClientModel.query.all():
-			clientList.append(client.dict())
-		return {"clients": clientList}
+		args = parser.parse_args()
+		print login_required(args['session'])
+		if (login_required(args['session'])):
+			clientList = []
+			page = 1
+			ipp = 10
+			paginate = ClientModel.query.paginate(page, ipp)
+			for client in ClientModel.query.all():
+				clientList.append(client.dict())
+			return {"clients": clientList}
+		else:
+			abort (403)
         
 	def post(self):
 		args = parser.parse_args()
@@ -72,10 +113,42 @@ class ClientsList(Resource):
 		db.session.add(client)
 		db.session.commit()
 		return client.dict(), 201
+		
+class Login(Resource):
+	def get(self):
+		args = parser.parse_args()
+		name = args['name']
+		pwd = args['pwd']
+		user = User.query.filter(User.username == name).first()
+		if (user):
+			if (user.verify_password(pwd)):
+				start = int(round(time.time()))
+				session = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(30))
+				s = Sess(start = start, session = session)
+				db.session.add(s)
+				db.session.commit()
+				return s.dict()
+			else:
+				abort (400, message = 'wrong password')
+		else:
+			abort (400, message = 'user doesn\'t exists')
+			
+	def post(self):
+		args = parser.parse_args()
+		username = args['name']
+		pwd = args['pwd']
+		if (len(User.query.filter(User.username == args['name']).all()) == 0):
+			user = User(username = username)
+			user.hash_password(pwd)
+			db.session.add(user)
+			db.session.commit()
+			return user.dict(), 201
+		else:
+			abort (400, message = 'user ' + username + ' already registered')
 
 api.add_resource(ClientsList, '/clients')
 api.add_resource(Client, '/clients/<int:client_id>')
-
+api.add_resource(Login, '/login')
 
 if __name__ == '__main__':
 	app.run(debug=True)
